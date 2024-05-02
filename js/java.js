44a9bd2a4da36b4a4c4091f056e31ca14610f230
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-01 22:23:47 -0600

    object.keys. getOwnPropertyNames, entries

--- js/java.js
+++ js/java.js
@@ -1,1 +1,17 @@
-console.log('Hellooooo');
+
+
+//metodos estaticos de prototipo:
+
+const estudiante1 = {
+    name: 'studiante1',
+    age: 18,
+    approveCourses: ['Curso 1'],
+    addCourse(newCourse){
+        this.approveCourses.push(newCourse)
+    }
+}
+
+
+console.log(Object.keys(estudiante1));
+console.log(Object.getOwnPropertyNames(estudiante1));
+console.log(Object.entries(estudiante1));
