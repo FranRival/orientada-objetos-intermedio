2428ee2aa40f8be90bf2163a717a8a65fa5755dd
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-01 22:35:30 -0600

    tratando,... no se.

--- js/java.js
+++ js/java.js
@@ -7,11 +7,15 @@ const estudiante1 = {
     age: 18,
     approveCourses: ['Curso 1'],
     addCourse(newCourse){
+        console.log('This', this);
+        console.log('This.approveCourses', this.approveCourses);
         this.approveCourses.push(newCourse)
     }
 }
 
 
-console.log(Object.keys(estudiante1));
-console.log(Object.getOwnPropertyNames(estudiante1));
-console.log(Object.entries(estudiante1));
+console.log(Object.keys(estudiante1)) //longitud, imprimiendo las propiedades
+console.log(Object.getOwnPropertyNames(estudiante1));//lo mismo de arriba
+console.log(Object.entries(estudiante1)); //array de arrays
+
+//Object.entries(estudiante1)[x][x]('xxxx')
