980a6ae29eb1a11bf93bc99bb928f5582e787280
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-02 21:56:20 -0600

    no estan siendo afectados. cada objeto es independiente

--- js/java.js
+++ js/java.js
@@ -1,83 +1,18 @@
 
 
-const estudiante1 = {
-    name: 'studiante1',
-    age: 18,
-    approveCourses: ['Curso 1'],
-    addCourse(newCourse){
-        console.log('This', this);
-        console.log('This.approveCourses', this.approveCourses);
-        this.approveCourses.push(newCourse)
-    }
+
+//shallowcopy
+
+const obj1 = {
+    a: 'a',
+    b: 'b',
+}
+
+const obj2 = {}
+
+for (prop in obj1){
+    obj2[prop] = obj1[prop]
 }
 
 
-
-Object.defineProperty(estudiante1, 'navegator', {
-    value: 'chrome',
-    writable: true,
-    enumerable: false,
-    configurable: true
-})
-
-
-Object.defineProperty(estudiante1, 'PruebaNASA', {
-    value: 'extraterrestres',
-    writable: true,
-    enumerable: true,
-    configurable: true
-})
-
-Object.defineProperty(estudiante1, 'editor', {
-    value: 'visualstudio',
-    writable: false,
-    enumerable: true,
-    configurable: true
-})
-
-Object.defineProperty(estudiante1, 'terminal', {
-    value: 'wls',
-    writable: true,
-    enumerable: true,
-    configurable: false
-})
-
-Object.defineProperty(estudiante1, 'extraterrestres', {
-    value: 'Ovnis alv',
-    writable: false,
-    enumerable: false,
-    configurable: false
-})
-
-
-Object.freeze(estudiante1) 
-
-
-console.log(Object.getOwnPropertyDescriptors(estudiante1));
-
-//Los objetos son referencias a un esapcio en memoria. 
-//cuando copiamos un objeto, estamos copiando su referencia en la memoria
-
-//memoria en js
-//inicializacion; crear una nueva variable. asignacion; esa variable tiene un valor.
-
-//2 valores; nombre de la variable, valor de cada una variable. no se guardan juntas. 
-
-//se guardan en stack. 
-
-//pero cuando son objetos: es un apuntador. HEAP.
-
-//objetos: referencias a un espacio en memoria.
-//js tiene stack y heap.
-//stack rapida pero sin espacio; nombres y valores de las variables,
-
-//objetos; apuntador a la memoria heap. 
-//solo es con objetos. 
-
-//gato1
-//gato2
-
-//si moficiamos uno de estos. madificamos tambien el otro. es el mismo objeto. 
-
-//copiar objetos para resolver problemas:
-//shallow copy. deepcopy. 
+//no afecta la copia del otro.
