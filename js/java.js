01c7abce4b386ea2980eb6fadab519118fc77db2
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-02 22:28:35 -0600

    no funcionan ambos stringfied y parse en metodos. los omite. no existen.

--- js/java.js
+++ js/java.js
@@ -5,6 +5,10 @@ const obj1 = {
     c: {
         d: 'd',
         e: 'e'
+    },
+    
+    editA(){
+        this.a = 'Amouranth the whore'
     }
 }
 
@@ -17,3 +21,9 @@ const obj2 = JSON.parse(stringifyCOmplexObject) //creamos un objeto a partir de 
 //JSON.stringify: permite convertir objetos en string.
 
 //afectando uno, el otro no se afecta. tampoco sus propiedades del objeto
+
+//convertirmos el obj1 en un string.
+//luego convertimos un string en un objeto. 
+//apunta a stack y luego a un nuevo heat
+
+//pero no funciona con metodos.
