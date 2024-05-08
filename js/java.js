8376ef0555e7d0e5ed342b6f4f40d86e1b15bab9
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-07 22:23:26 -0600

    se arreglo el desmadre, tenia atributos en parentesis

--- js/java.js
+++ js/java.js
@@ -15,72 +15,13 @@ function isArray(subject){
 
 
 
-function deepCopy(subject){
-    let copySubject;
-
-    const subjectIsArray = isArray(subject)
-    const subjectIsObject = isObject(subject)
-
-    if (subjectIsArray) {
-        copySubject = []
-    }else if (subjectIsObject) {
-        copySubject = {}
-    }else{
-        return subject
-    }
-
-
-    for (key in subject){
-        const keyIsObject = isObject(subject[key])
-
-        if (keyIsObject) {
-            copySubject[key] = deepCopy(subject[key])
-            
-        }else {
-            if (subjectIsArray) {
-                copySubject.push(subject[key])
-            }else{
-                copySubject[key] = subject[key]
-            }
-        }
-    }
-    return copySubject
-}
-
-
-const studentBase = {
-    name: undefined,
-    email: undefined,
-    age: undefined,
-    aprovedCourses: undefined,
-    learningPaths: undefined,
-    socialMedia: {
-        twitter: undefined,
-        instagram: undefined,
-        facebook: undefined
-    }
-}
-
-
-
-const Zara = deepCopy(studentBase)
-
-Object.seal(Zara) 
-
-Object.isSealed(Zara) //preguntarle a la consola si todos estan protegidos.
-Object.isFrozen(Zara) //no poder editar. 
-
-
-
-
-
 
 function requiereParam (param){
     throw new Error(param + ' parametro obligatorio');
 }
 
 function createStudent ({
-    name = requiereParam('name'), //obligatorio
+    name = requiereParam('name'), 
     email = requiereParam('email'),
     age,
     twitter,
@@ -105,8 +46,8 @@ function createStudent ({
             instagram,
         },
 
-        readName(newName){ 
-            return private["_name"] = newName
+        readName(){ 
+            return private["_name"]
         },
 
         changeName(newName){ //tenemos que pasar por la funcion. 
