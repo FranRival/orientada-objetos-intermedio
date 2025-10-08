79006f1f5c51ca751a4e6ae5685b50893b91574c
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2025-10-07 21:26:56 -0600

    getter y setter

--- js/java.js
+++ js/java.js
@@ -272,4 +272,4 @@ Object.isFrozen(Zara)
 ////-----
 ///no me acuerdo que son los metodos. o si los puedo crear yo. 
 
-//ejecutando el deepcopy.
+//ejecutando el deepcopy. entendiendo el getter y setter dentro de object.descriptor.
