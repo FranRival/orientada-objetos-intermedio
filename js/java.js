cb2f847efa05caea68fdc40a511c4d17a78c0fc2
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2025-10-08 21:14:23 -0600

    private public - getter y setter en createStudent.

--- js/java.js
+++ js/java.js
@@ -272,4 +272,4 @@ Object.isFrozen(Zara)
 ////-----
 ///no me acuerdo que son los metodos. o si los puedo crear yo. 
 
-//ejecutando el deepcopy. entendiendo el getter y setter dentro de object.descriptor. factoryFunction con createStudent. probando la funcion de fatory function.
+//ejecutando el deepcopy. entendiendo el getter y setter dentro de object.descriptor. factoryFunction con createStudent. probando la funcion de fatory function. las variables public y private estan funcionando como getters y setters + objectDescriptors.
